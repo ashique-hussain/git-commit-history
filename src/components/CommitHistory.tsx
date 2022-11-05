5e71af61764ff407ec2a46d5049420dfe6d680be
ashique-hussain <ashique9694@gmail.com>
2022-11-05 15:50:11 +0530

    feat: commit history api integrated

--- src/components/CommitHistory.tsx
+++ src/components/CommitHistory.tsx
@@ -1,3 +1,5 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import { useEffect, useState } from "react";
 import CommitCard from "./views/CommitCard";
 
@@ -8,17 +10,24 @@ const CommitHistory = () => {
     fetch(
       `https://api.github.com/repos/ashique-hussain/git-commit-history/commits`
     )
-      .then((res: { [key: string]: any }) => {
-        setCommit(res.data);
+      .then(async (response: any) => {
+        const commitHistory = await response.json();
+        setCommit(commitHistory);
       })
-      .catch((error) => {});
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   return (
-    <div>
-      {JSON.stringify(commit)}
-      <CommitCard />
-    </div>
+    <Box>
+      <Typography component="h2" variant="h4">
+        Commit History
+      </Typography>
+      {commit.map(({ commit, sha }) => (
+        <CommitCard key={sha} commit={commit} />
+      ))}
+    </Box>
   );
 };
 
--- src/components/views/CommitCard.tsx
+++ src/components/views/CommitCard.tsx
@@ -2,17 +2,24 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 
-interface IProps {}
+interface IProps {
+  commit: { [key: string]: any };
+}
 
 const CommitCard = (props: IProps) => {
+  const {
+    author: { name, date },
+    message,
+    url,
+  } = props.commit;
   return (
-    <Card elevation={2}>
+    <Card elevation={2} sx={{ my: 1.5 }}>
       <CardContent>
         <Typography variant="h5" component="div">
-          Commit message
+          {message}
         </Typography>
         <Typography variant="body2" sx={{ pt: 0.5 }}>
-          July 21, 01:12PM by Ashique Ansari
+          {date} by {name}
         </Typography>
       </CardContent>
     </Card>
